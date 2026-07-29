6c34dce793ff4016eacac0932d59023e6f93c70d
tmoleary21 <olearytyson@gmail.com>
2026-07-29 17:19:38 -0600

    Attempted fix to install looping problem

--- wallmonitor/install.sh
+++ wallmonitor/install.sh
@@ -13,21 +13,26 @@ mkdir -p "$app_dir"
 
 script_url="$latest_release/install.sh"
 if wget -O new-install.sh "$script_url" >> $log_file; then
-  new_release_tag=$(sed -n '1s/^# Release: //p' new-install.sh)
+  new_release_tag=$(sed -n '1s/^# Release: //p' ./new-install.sh)
   echo $new_release_tag >> $log_file
-  this_release_tag=$(sed -n '1s/^# Release: //p' install.sh)
+  this_release_tag=$(sed -n '1s/^# Release: //p' ./install.sh)
   echo $this_release_tag >> $log_file
 
   if [ "$new_release_tag" != "$this_release_tag" ]; then
     echo "install script updated" >> $log_file
-    # mv new-install.sh "$app_dir"
-    script_path="$app_dir/install.sh"
+
+    mv new-install.sh "$app_dir"
+    pushd "$app_dir" > /dev/null
+    script_path="./install.sh"
+
     if [ -e "$script_path" ]; then
-      mv "$script_path" "$app_path/old-install.sh"
+      mv "$script_path" "./old-install.sh"
     fi
     mv new-install.sh "$script_path"
     chmod +x "$script_path"
-    exec "$script_path" "$@" # Runs instead. Replaces running script
+
+    exec "./install.sh; popd > /dev/null" "$@" # Runs instead. Replaces running script
+
   else 
     echo "no install script update" >> $log_file
     rm new-install.sh
@@ -69,8 +74,9 @@ python3 -m http.server 8000 --bind 127.0.0.1 &
 cd -
 
 # Stark kiosk mode
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
-XDG_RUNTIME_DIR=/run/user/$(id -u) cage -s -d -- chromium --kiosk --noerrdialogs --no-first-run --disable-infobars \
+cage -s -d -- chromium --enable-features=UseOzonePlatform --ozone-platform=wayland --kiosk --noerrdialogs --no-first-run --disable-infobars \
   --disable-session-crashed-bubble --disable-features=TranslateUI \
   --check-for-update-interval=31536000 \
   --app=http://127.0.0.1:8000
